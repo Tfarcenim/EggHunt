26bf45a498f8dafecaad8c6f3c969e3053aeb621
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-03-16 19:27:50 -0400

    doesn't work

--- forge/src/main/java/tfar/egghunt/datagen/ModBlockModelProvider.java
+++ forge/src/main/java/tfar/egghunt/datagen/ModBlockModelProvider.java
@@ -13,6 +13,8 @@ import net.minecraft.world.level.block.Block;
 import tfar.egghunt.EggHunt;
 import tfar.egghunt.init.ModBlocks;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -33,23 +35,37 @@ public class ModBlockModelProvider extends BlockModelGenerators {
         createTrivialBlock1(ModBlocks.EASTER_EGG);
     }
 
+    private static final int variants = 4;
+    private static final int sub_variants = 5;
+
     public void createTrivialBlock1(Block pBlock) {
         //TexturedModel.CUBE.create(pBlock, this.modelOutput);
 
         ResourceLocation resourcelocation = new ResourceLocation(EggHunt.MOD_ID,"block/easter_egg");
         ModelTemplate template = create(resourcelocation,TextureSlot.ALL,TextureSlot.PARTICLE);
-        TexturedModel.Provider provider = TexturedModel.createDefault(pBlock1 ->
-                withTexture(new ResourceLocation(EggHunt.MOD_ID,"block/easter_egg1-1")), template);
-        provider.createWithSuffix(pBlock,"1-1",modelOutput);
 
-        this.blockStateOutput.accept(createSimpleBlock1(pBlock, resourcelocation));
+        List<ResourceLocation> text_variants = new ArrayList<>();
+
+        for (int i = 1; i < variants+1;i++) {
+            for (int j = 1; j < sub_variants+1;j++) {
+                ResourceLocation var = resourcelocation.withSuffix(i +"-"+j);
+
+                text_variants.add(var);
+
+                TexturedModel.Provider provider = TexturedModel.createDefault(pBlock1 ->
+                        withTexture(var), template);
+                provider.createWithSuffix(pBlock,i +"-"+j,modelOutput);
+            }
+        }
+
+
+
+        this.blockStateOutput.accept(createSimpleBlock1(pBlock,text_variants));
     }
 
-    public static MultiVariantGenerator createSimpleBlock1(Block pBlock, ResourceLocation baseModel) {
-        ResourceLocation model11 = baseModel.withSuffix("1-1");
-        ResourceLocation model12 = baseModel.withSuffix("1-2");
-        return MultiVariantGenerator.multiVariant(pBlock, Variant.variant().with(VariantProperties.MODEL, model11),
-                Variant.variant().with(VariantProperties.MODEL, model12));
+    public static MultiVariantGenerator createSimpleBlock1(Block pBlock,List<ResourceLocation> variants) {
+        Variant[] variants1 = variants.stream().map( r -> Variant.variant().with(VariantProperties.MODEL,r)).toArray(i -> new Variant[i]);
+        return MultiVariantGenerator.multiVariant(pBlock, variants1);
     }
 
     public static final ModelTemplate EASTER_EGG_TEMPLATE = create(new ResourceLocation(EggHunt.MOD_ID,"block/easter_egg"), TextureSlot.ALL,TextureSlot.PARTICLE);
