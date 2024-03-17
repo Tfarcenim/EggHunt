5ceb8cd59f0b007aa3221c26baee4109e7fe8aad
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-03-16 20:30:58 -0400

    add cloth config

---
+++ common/src/main/java/tfar/egghunt/EggHuntData.java
@@ -0,0 +1,11 @@
+package tfar.egghunt;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.saveddata.SavedData;
+
+public class EggHuntData extends SavedData {
+    @Override
+    public CompoundTag save(CompoundTag var1) {
+        return var1;
+    }
+}
